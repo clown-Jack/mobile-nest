4cb9b88664bfa9761bb7fa62b52b76207e66a7fe
songjie <18510802912@163.com>
2020-06-04 17:45:08 +0800

    login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Post, Redirect, Query, Param, } from '@nestjs/common';
+import {Request} from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,37 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller()
+export class LoginController {
+  /**
+   * 查看请求参数
+   * @param requset 
+   */
+  @Get('login')
+  login(@Req() requset:Request):object {
+    // console.log(requset);
+    return {code:0,msg:"登陆成功"}
+  }
+
+  /**
+   * 重定向
+   * @param vension 
+   */
+  @Get('docs')
+  @Redirect("https://docs.nestjs.com",302)
+  docs(@Query('vension') vension){
+    if(vension&&vension=="5"){
+      return {url:'https://docs.nestjs.com/v5/'}
+    }else{
+      return "aa"
+    }
+  }
+
+  @Get('detail/id')
+  detail(@Param("id") id):string{
+    return `this is ${id}`
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController,LoginController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController,LoginController],
   providers: [AppService],
 })
 export class AppModule {}
